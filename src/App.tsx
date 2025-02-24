6cd6e1c4cf4391b6bb13683b6e678cb11b3b3212
Kithmina Kaluarachchi <kithmina.20230197@iit.ac.lk>
2025-02-24 16:25:33 +0530

    Fixed scroll to the top

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,9 @@ export function App() {
       <main className="relative z-[1]">
         <Router>
           <ScrollToTop />
-          <div className="w-full min-h-screen bg-white">
+          <div className="w-full min-h-screen bg-white pt-16 md:pt-0">
+            {" "}
+            {/* Add padding top for mobile */}
             <Navbar />
             <Routes>
               <Route path="/" element={<Home />} />
